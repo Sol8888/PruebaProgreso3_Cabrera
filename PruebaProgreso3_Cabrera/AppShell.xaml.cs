bed3f599d195e2c8d12dab0173ba206a1865bb31
Sol8888 <daniela.cabrera.tapia@udla.edu.ec>
2025-01-24 07:07:29 -0500

    Agregar archivos de proyecto.

---
+++ PruebaProgreso3_Cabrera/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PruebaProgreso3_Cabrera
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PruebaProgreso3_Cabrera/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PruebaProgreso3_Cabrera
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
