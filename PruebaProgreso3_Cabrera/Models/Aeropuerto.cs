a95b78dce284595a1affb6fb9b1b10e72e547f85
Sol8888 <daniela.cabrera.tapia@udla.edu.ec>
2025-01-24 08:04:00 -0500

    Creacion del modelos Aeropuerto

---
+++ PruebaProgreso3_Cabrera/Models/Aeropuerto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaProgreso3_Cabrera.Models
+{
+    public class Aeropuerto
+    {
+        public string Nombre { get; set; }
+        public string Pais { get; set; }
+        public double Latitud { get; set; }
+        public double Longitud { get; set; }
+        public string Correo { get; set; }
+        public string Scabrera { get; set; } = "SCabrera";
+    }
+}
